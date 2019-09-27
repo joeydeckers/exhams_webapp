b992a3dba8587d979e05c6572bb8087648778ec6
JoeyDeckers <deckersjoey@gmail.com>
2019-09-27 19:00:51 +0200

    First version of landing done

--- src/components/NavbarLanding.vue
+++ src/components/NavbarLanding.vue
@@ -1,6 +1,6 @@
 <template>
   <div id ="navbarLanding">
-    <b-navbar :style = "{backgroundColor: navbarBackgroundColor}" toggleable="lg" type="dark" variant="info">
+    <b-navbar class="fixed-top " :style = "{backgroundColor: navbarBackgroundColor}" toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">Exhams</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
@@ -8,8 +8,9 @@
         <b-navbar-nav class="ml-auto">
             <!-- Using 'button-content' slot -->
             <b-navbar-nav>
-                <b-nav-item class="accent-color" href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                <b-nav-item href="#">Over Exhams</b-nav-item>
+                <b-nav-item href="#">Contact</b-nav-item>
+                <b-nav-item class="accent-color" href="#">Wordt een trainer</b-nav-item>
             </b-navbar-nav>
         </b-navbar-nav>
       </b-collapse>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="home">
+  <div class="landing">
     <NavbarLanding navbarBackgroundColor = "transparent !important"/>
+    <section class="intro">
+      <b-container fluid>
+        <b-row>
+          <b-col>
+            <div class="intro-text">
+             <h2>Het <strong>online</strong> tentamentraining platform waardoor <strong>jij</strong> je tentamen haalt</h2>
+             <p>Zoek hier je tentamentraining die past bij jouw opleiding en universiteit.</p>
+             <div class="search">
+               <div class="select">
+                <i class="fas fa-school"></i>
+                <select name="" id="">
+                  <option value="">Kies universiteit</option>
+                  <option value="">Kies universiteit</option>
+                  <option value="">Kies universiteit</option>
+                  <option value="">Kies universiteit</option>
+                  <option value="">Kies universiteit</option>
+                </select>
+               </div>
+               <div class="select">
+                <select name="" id="">
+                  <option value="">Kies studie</option>
+                </select>
+               </div>
+               <button class="btn btn-custom">Zoeken</button>
+             </div>
+           </div>
+          </b-col>
+          <b-col>
+            <div class="landing-bg"></div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
   </div>
 </template>
 
