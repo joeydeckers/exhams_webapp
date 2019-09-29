2008dfb01f68949475b9d3bb74156192a9b79ae6
JoeyDeckers <deckersjoey@gmail.com>
2019-09-29 23:12:47 +0200

    Started with login

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-container>
+            <div class="login-form">
+                <input class="form-control" type="text">
+                <input class="form-control" type="password">
+                <button class="btn ">Inloggen</button>
+            </div>
+        </b-container>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
