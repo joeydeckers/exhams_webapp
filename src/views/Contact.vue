892803f93276417403543c8038c2666b9c5b34a3
JoeyDeckers <deckersjoey@gmail.com>
2019-10-02 19:17:20 +0200

    Finished ui of contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,28 +5,28 @@
         </div>
         <b-container>
             <b-row> 
-                <b-col lg="6">
-                    <h2>Heb je vragen? Neem gerust <br>contact op!</h2>
-                    <p>Wij zullen je bericht zo snel mogelijk behandelen.</p>
+                <b-col lg="4">
+                    <h2><strong>Heb je vragen? Neem gerust <br>contact op!</strong></h2>
+                    <p>Wij nemen contact met onze gebruikers erg serieus. Mocht je vragen of suggesties hebben over ons platform aarzel niet! Wij zullen zo snel mogelijk op je reageren.</p>
                     <div>
                         <p>Email: info@exhams.com</p>
                         <p>Tel: 112</p>
                     </div>
                 </b-col>
-                <b-col offset-lg="1" lg="5">
+                <b-col offset-lg="3" lg="5">
                     <form action="">
                         <b-row>
                             <b-col lg="12">
                                 <p>Jouw naam</p>
-                                <input class="form-control" type="text">
+                                <input v-model="name" class="form-control" type="text">
                             </b-col>
                             <b-col lg="12">
                                 <p>Jouw email</p>
-                                <input class="form-control" type="text">
+                                <input v-model="email" class="form-control" type="text">
                             </b-col>
                             <b-col lg="12">
                                 <p>Jouw bericht</p>
-                                <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
+                                <textarea v-model="message" class="form-control" name="" id="" cols="30" rows="10"></textarea>
                                 <Button style="margin-top:10px;" buttontext="bericht versturen"/>
                             </b-col>
                         </b-row>
@@ -46,6 +46,13 @@ export default {
     components:{
         NavbarLanding,
         Button
+    },
+    data(){
+        return{
+            name: '',
+            email: '',
+            message: ''
+        }
     }
 }
 </script>
